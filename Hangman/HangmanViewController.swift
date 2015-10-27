e5531dd72eabe57c08ccbfa8c2d1a3fcce0784b0
Luis <alternateluis@live.com>
2015-10-26 23:42:22 -0700

    hangman

--- Hangman/HangmanViewController.swift
+++ Hangman/HangmanViewController.swift
@@ -8,20 +8,46 @@
 
 import UIKit
 
-class HangmanViewController: UIViewController {
+class HangmanViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var guessField: UITextField!
    
     @IBAction func guess(sender: AnyObject) {
-    
+        if (guessField.text != "") {
+            let guess = guessField.text
+            guessField.text = ""
+            if (guessesMade.text == "Guesses") {
+                guessesMade.text = "Guesses\n" + guess! + "\n"
+            }
+            else {
+                guessesMade.text = guessesMade.text! + guess! + "\n"
+            }
+
+        }
+        self.view.endEditing(true)
     }
     
     @IBAction func newGame(sender: AnyObject) {
+        guessField.text = ""
+        guessesMade.text = "Guesses"
     }
+    
+    
     @IBOutlet weak var guessesMade: UILabel!
+    
+    @IBOutlet weak var screen: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let path = NSBundle.mainBundle().pathForResource("words", ofType: "plist")
+        self.guessField.delegate = self
+        NSLog(path!)
+        
+        let image: UIImage = UIImage(named: "hangman.png")!
+        screen.image = image
+//        screen!.frame = CGRectMake(0,0,100,200)
+//        self.view.addSubview(bgImage!)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -29,5 +55,12 @@ class HangmanViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+
+    
 }
 
